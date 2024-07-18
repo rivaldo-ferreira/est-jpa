b6cc71deac4f2cb86c4692288e58321fe0d36dd6
rivaldo-ferreira <rivaldo13@gmail.com>
2024-07-18 09:26:46 -0300

    Live sobre JPA Repository

---
+++ src/main/java/com/estudo_jpa/repositorio/RepositorioApplication.java
@@ -0,0 +1,15 @@
+package com.estudo_jpa.repositorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioApplication {
+	//https://www.youtube.com/watch?v=jh_T5_o3qKE ---- 48:42
+	//https://github.com/devsuperior/aula-jparepository
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioApplication.class, args);
+	}
+
+}
